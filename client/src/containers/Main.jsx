dcfa573267e7271946a77de27348de1c0bbabe5c
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-11 23:10:49 +0000

    changing title

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -6,7 +6,7 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>FreeAgent for Ebay</h1>
+        <h1>Freebay Agent</h1>
         <nav>
           <Link className='link' to="/ebay">Ebay</Link>
           <Link className='link' to="/free-agent">FreeAgent</Link>
