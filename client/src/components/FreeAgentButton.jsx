e6735a55edaf103afddead27384fc372334acae6
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-11 22:30:18 +0000

    FreeAgent button enabled

--- client/src/components/FreeAgentButton.jsx
+++ client/src/components/FreeAgentButton.jsx
@@ -8,18 +8,22 @@ const FreeAgentButton = function({ price, description }) {
   var token = '1mnnp7BmgRFQcpliSQRR_yQ_2l0_jh02_SlOixFgu'
 
   const handleClick = function() {
-    var expense = {
-      price: price,
-      description: description
+    var payload = {
+      expense: {
+        description: description[0],
+        category: "https://api.sandbox.freeagent.com/v2/categories/271",
+        currency: "GBP",
+        dated_on: "2017-02-14",
+        gross_value: (-price).toString(),
+        user: "https://api.sandbox.freeagent.com/v2/users/1560"
+      }
     }
-    // XmlHttpHelper.postWithAuthorizationToken(
-    //   url,
-    //   token,
-    //   {
-    //
-    //   }
-    // )
-    console.log( "creating expense:", expense );
+    console.log("expense:", payload);
+    XmlHttpHelper.postWithAuthorizationToken(
+      url,
+      token,
+      payload
+    )
   }
 
   return (
