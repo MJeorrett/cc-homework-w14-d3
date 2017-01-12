af0eb05fd6ff02360c9d1f13ca661fc176666c2d
Matthew Jeorrett <matthewjeorrett@gmail.com>
2017-01-12 09:16:59 +0000

    fixed position of controls container

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -6,12 +6,14 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Freebay Agent</h1>
-        <nav>
-          <Link className='link' to="/ebay">Ebay</Link>
-          <Link className='link' to="/free-agent">FreeAgent</Link>
-        </nav>
-        <hr />
+        <div id="controls-container">
+          <h1>Freebay Agent</h1>
+          <nav>
+            <Link className='link' to="/ebay">Ebay</Link>
+            <Link className='link' to="/free-agent">FreeAgent</Link>
+          </nav>
+        </div>
+        <div id="controls-container-padding"></div>
         <div>
           { this.props.children }
         </div>
